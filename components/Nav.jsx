74c3594dc545a379854471f0fb8a4317c7059e97
Nechar Joshi <joshinechar@gmail.com>
2019-11-07 21:41:23 +1100

    [minor] logo change

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,7 +1,7 @@
 const Nav = () => (
   <nav className="navbar navbar-light bg-light">
     <a className="navbar-brand" href="#">
-      <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" className="d-inline-block align-top" alt="" />
+      <img src="/logo.jpg" width="30" height="30" className="d-inline-block align-top" alt="" />
       Airports
     </a>
   </nav>
